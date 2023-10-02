7f8d8b636cc19627d4b260fa488d6057fbfb60ed
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-02 15:08:50 -0400

    phase-1-context-lab/
    phase-1-context-lab/

--- index.js
+++ index.js
@@ -1,59 +1,8 @@
-/* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
- function createEmployeeRecords(employeeData) {
-  return employeeData.map(employee => createEmployeeRecord(employee));
-}
-
-// Rest of the functions...
-
-module.exports = {
-  createEmployeeRecord,
-  createEmployeeRecords,
-  createTimeInEvent,
-  createTimeOutEvent,
-  hoursWorkedOnDate,
-  wagesEarnedOnDate,
-  allWagesFor,
-  calculatePayroll,
-};
-
- function createEmployeeRecord(employeeInfo) {
-  return {
-    firstName: employeeInfo[0],
-    familyName: employeeInfo[1],
-    title: employeeInfo[2],
-    payPerHour: employeeInfo[3],
-    timeInEvents: [],
-    timeOutEvents: []
-  };
-}
-
-function createTimeInEvent(dateStamp) {
-  const [date, hour] = dateStamp.split(" ");
-  this.timeInEvents.push({
-    type: "TimeIn",
-    hour: parseInt(hour),
-    date: date
-  });
-  return this;
-}
-
-function createTimeOutEvent(dateStamp) {
-  const [date, hour] = dateStamp.split(" ");
-  this.timeOutEvents.push({
-    type: "TimeOut",
-    hour: parseInt(hour),
-    date: date
-  });
-  return this;
+function hoursWorkedOnDate(date) {
+  const timeInEvent = this.timeInEvents.find(event => event.date === date);
+  const timeOutEvent = this.timeOutEvents.find(event => event.date === date);
+  const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+  return hoursWorked;
 }
 
 function hoursWorkedOnDate(date) {
@@ -69,18 +18,8 @@ function wagesEarnedOnDate(date) {
   return wagesEarned;
 }
 
-function allWagesFor() {
-  const datesWorked = this.timeInEvents.map(event => event.date);
-  const totalWages = datesWorked.reduce(
-    (total, date) => total + wagesEarnedOnDate.call(this, date),
-    0
-  );
-  return totalWages;
-}
-
-function calculatePayroll(employees) {
-  return employees.reduce(
-    (totalPayroll, employee) => totalPayroll + allWagesFor.call(employee),
-    0
-  );
+const wagesEarnedOnDate = function (date) {
+  const hoursWorked = this.hoursWorkedOnDate(date);
+  const wagesEarned = hoursWorked * this.payPerHour;
+  return wagesEarned;
 }
