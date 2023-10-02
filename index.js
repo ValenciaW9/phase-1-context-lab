60d9f463382c26e4b953db7c69608630f74bc982
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-02 16:45:06 -0400

    phase-1-context-lab/
    phase-1-context-lab/

--- index.js
+++ index.js
@@ -14,15 +14,12 @@ function createEmployeeRecord(employeeData) {
 function createEmployeeRecords(arraysOfEmployeeData) {
   return arraysOfEmployeeData.map(createEmployeeRecord);
 }
+
+// Create a function to add a time-in event to an employee's record
 function createTimeInEvent(employeeRecord, dateStamp) {
   if (typeof dateStamp !== 'string' || dateStamp.length !== 16) {
     throw new Error('Invalid dateStamp');
   }
-
-  const [date, hour] = dateStamp.split(" ");
-  employeeRecord.timeInEvents.push({ type: "TimeIn", hour: parseInt(hour, 10), date: date });
-  return employeeRecord;
-}
 
   const [date, hour] = dateStamp.split(" ");
   employeeRecord.timeInEvents.push({
@@ -31,49 +28,19 @@ function createTimeInEvent(employeeRecord, dateStamp) {
     date: date
   });
   return employeeRecord;
-}function createTimeOutEvent(employeeRecord, dateStamp) {
+}
+
+// Create a function to add a time-out event to an employee's record
+function createTimeOutEvent(employeeRecord, dateStamp) {
   if (typeof dateStamp !== 'string' || dateStamp.length !== 16) {
     throw new Error('Invalid dateStamp');
   }
 
   const [date, hour] = dateStamp.split(" ");
-  employeeRecord.timeOutEvents.push({ type: "TimeOut", hour: parseInt(hour, 10), date: date });
+  employeeRecord.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date: date
+  });
   return employeeRecord;
-}
-
-// Create a function to calculate hours worked on a specific date
-function hoursWorkedOnDate(employeeRecord, date) {
-  const timeInEvent = employeeRecord.timeInEvents.find(event => event.date === date);
-  const timeOutEvent = employeeRecord.timeOutEvents.find(event => event.date === date);
-
-  const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
-  return hoursWorked;
-}
-
-// Create a function to calculate wages earned on a specific date
-function wagesEarnedOnDate(employeeRecord, date) {
-  const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
-  const payRate = employeeRecord.payPerHour;
-  return hoursWorked * payRate;
-}
-
-// Create a function to calculate all wages for an employee
-function allWagesFor(employeeRecord) {
-  const datesWorked = employeeRecord.timeInEvents.map(event => event.date);
-  const totalWages = datesWorked.reduce((total, date) => {
-    return total + wagesEarnedOnDate(employeeRecord, date);
-  }, 0);
-  return totalWages;
-}
-
-// Create a function to find an employee by first name
-function findEmployeeByFirstName(srcArray, firstName) {
-  return srcArray.find(employee => employee.firstName === firstName);
-}
-
-// Create a function to calculate the payroll for all employees
-function calculatePayroll(employeeRecords) {
-  return employeeRecords.reduce((totalPayroll, employeeRecord) => {
-    return totalPayroll + allWagesFor(employeeRecord);
-  }, 0);
 }
