48a5040a77d29df83dcc6121c74f7f5f6e3fb798
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-02 15:29:12 -0400

    phase-1-context-lab/
    phase-1-context-lab/

--- index.js
+++ index.js
@@ -5,21 +5,8 @@ function hoursWorkedOnDate(date) {
   return hoursWorked;
 }
 
-function hoursWorkedOnDate(date) {
-  const timeInEvent = this.timeInEvents.find(event => event.date === date);
-  const timeOutEvent = this.timeOutEvents.find(event => event.date === date);
-  const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
-  return hoursWorked;
-}
-
 function wagesEarnedOnDate(date) {
   const hoursWorked = hoursWorkedOnDate.call(this, date);
-  const wagesEarned = hoursWorked * this.payPerHour;
-  return wagesEarned;
-}
-
-const wagesEarnedOnDate = function (date) {
-  const hoursWorked = this.hoursWorkedOnDate(date);
   const wagesEarned = hoursWorked * this.payPerHour;
   return wagesEarned;
 }
